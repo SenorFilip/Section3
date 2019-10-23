968168563098d23f1fab846ff62f99a77929922c
senorFilip <filipcicfilip1@gmail.com>
2019-10-23 10:56:08 +0200

    Sirovo napravljen prikaz recepata i namirnica

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://live.staticflickr.com/4891/45165261135_69cb589907_b.jpg'),
+    new Recipe(
+      'A Test Recipe1',
+      'This is simply a test1',
+      'https://live.staticflickr.com/4891/45165261135_69cb589907_b.jpg')
+  ];
+
+  constructor() {  }
 
   ngOnInit() {
   }
